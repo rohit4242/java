f70fc497bec325bca70fe565e7d8e583fdf8674e
Rohit Luni <rohitluni4242@gmail.com>
2022-07-26 15:29:30 +0530

    inti

--- basic_programs/Unit-1/program_05.java
+++ basic_programs/Unit-1/program_05.java
@@ -7,17 +7,48 @@ public class program_05 {
         Scanner obj = new Scanner(System.in);
 
         int[] sub = new int[3];
-        int total=0;
+        float total=0;
 
         for(int i=0;i<3;i++)
         {
-            System.out.printf("Enter Subject marks %d",i);
+            System.out.printf("Enter Subject marks %d: ",i);
             sub[i] = obj.nextInt();
             total += sub[i];
         }
 
-        int per = (total*3)/100;
+        float per = (float)(total/3.0);
 
+        System.out.println(total);
         
+        System.out.println("A Total Of 3 Subject is :"+total);
+        
+        System.out.println("A Percentage is :"+per);
+        
+        if(total<100)
+        {
+            System.out.println("Student is Fail");
+        }
+        
+        else
+        {
+            System.out.printf("\nStudent is pass with %d Percentage",per);
+            
+        }
+        
+        if(total<100)
+        {
+            System.out.println("\nStudent Get C Class");
+        }
+        
+        else if(total<=150)
+        {
+            System.out.println("\nStudent Get B Class");
+        }
+        
+        else
+        {
+            System.out.println("\nGet A Class");
+        }
+        obj.close();
     }
 }
