1ff28c2df9df67ca50713707c55b6be7f4c512b6
rohit4242 <rohitluni4242@gmail.com>
2022-07-30 20:57:05 +0530

    init

---
+++ basic_programs/Unit-3/program_23.java
@@ -0,0 +1,26 @@
+// 3. Write a Java program to demonstrate use of Hierarchical inheritance
+
+class a{
+    void displayA(){
+        System.out.println("a");
+    }
+}
+class b extends a{
+    void displayB(){
+        System.out.println("b");
+    }
+}
+class c extends a,b{
+    void displayC(){
+        System.out.println("c");
+    }
+}
+class program_23{
+    public static void main(String[] args)
+    {
+        c obj = new c();
+        obj.displayA();
+        obj.displayB();
+        obj.displayC();
+    }
+}
